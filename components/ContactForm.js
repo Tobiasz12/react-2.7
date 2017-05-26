06123fd84272f6b36e78f2658a1a5c69d5245261
Tobiasz12 <tobiasz63@op.pl>
2017-05-26 18:55:31 +0200

    Task 2.5

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -5,12 +5,12 @@ var ContactForm = React.createClass({
 
   render: function() {
     return (
-      <form className = {'contactForm'}>
-        <input type = 'text' placeholder = 'Imię' value = this.props.contact.fristName/>
-        <input type = 'text' placeholder = 'Nazwisko' value = this.props.contact.lastName/>
-        <input type = 'email' placeholder = 'Email' value = this.props.contact.email/>
-        <button type = 'submit'>Dodaj kontakt</button>
+      <form className = 'contactForm'>
+        <input type='text' placeholder='Imię' value= {this.props.contact.firstName}/>
+        <input type='text' placeholder='Nazwisko' value= {this.props.contact.lastName}/>
+        <input type='email' placeholder='Email' value= {this.props.contact.email}/>
+        <button type='submit' placeholder='Email'>Dodaj kontakt</button>
       </form>
-    )
+    );
   },
-})
+});
